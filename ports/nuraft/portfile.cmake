21c985060b9eb4f290495f2246ac2571410b1832
lira <lithrad@gmail.com>
2025-11-22 16:10:43 +0800

    Clean up obsolete code of nuraft port

--- ports/nuraft/portfile.cmake
+++ ports/nuraft/portfile.cmake
@@ -1,5 +1,3 @@
-set(VCPKG_LIBRARY_LINKAGE static)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO eBay/NuRaft
@@ -7,27 +5,21 @@ vcpkg_from_github(
     SHA512 16baaa9921228c48bfee2aa795b0c644228ceeae32430d2782593dd8087978359edcf47e17e551fbf475df22b127097d8d149fc0996c9ade7b5ae7bafd183f62
     HEAD_REF master
     PATCHES
-        0001-fix-not-keyword.patch
-        0002-fix-cmake-build.patch
+        fix-build-compatibility-issues.patch
 )
 
-#vcpkg_from_github(
-#    OUT_SOURCE_PATH ASIO_SOURCE
-#    REPO chriskohlhoff/asio
-#    REF "asio-1-34-0"
-#    SHA512 989e1b453cd5ab3cd8d9d35ea828c6fefb539b41c5e7f57b1dcba9a0a0f1cb2f90a80b4e03cc071fc904e2cf82212e6afb29062d50c2ebf36e798ce171f3ed48
-#    HEAD_REF master
-#)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
-    DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         -DBUILD_EXAMPLES=OFF
-#       -DASIO_INCLUDE_DIR=${ASIO_SOURCE}
+        -DBUILD_TESTING=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/NuRaft)
 
