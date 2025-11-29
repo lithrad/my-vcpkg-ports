a762455d9d7b38c9ddc19c6eccf023a5c3bd3967
lira <lithrad@gmail.com>
2025-11-29 12:08:59 +0800

    Revise deprecated helper functions and optimize build fix logic

--- ports/nuraft/portfile.cmake
+++ ports/nuraft/portfile.cmake
@@ -4,26 +4,28 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 16baaa9921228c48bfee2aa795b0c644228ceeae32430d2782593dd8087978359edcf47e17e551fbf475df22b127097d8d149fc0996c9ade7b5ae7bafd183f62
     HEAD_REF master
-    PATCHES
-        fix-build-compatibility-issues.patch
 )
 
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+    set(msvc_options "-DCMAKE_CXX_FLAGS=\"/std:c++latest\"")
 endif()
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    WINDOWS_USE_MSBUILD
     OPTIONS
         -DBUILD_EXAMPLES=OFF
         -DBUILD_TESTING=OFF
+        ${msvc_options}
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/NuRaft)
 
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
-
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
